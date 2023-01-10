583fdf1f55f0c098128aed940c272123deadf430
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2023-01-10 13:13:07 -0800

    v1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-const init = () => {
-    console.log('loaded')
-}
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
-document.addEventListener("DOMContentLoaded", init)
+gsap.registerPlugin(MotionPathPlugin);
+
+let tween;
+
+const initMotionPath = () => {
+  let progress = tween ? tween.progress() : 0;
+  tween && tween.progress(0).kill();
+
+  tween = gsap.to("#dot", {
+    motionPath: {
+      path: "#path",
+      align: "#path",
+      alignOrigin: [0.5, 0.5],
+      autoRotate: true,
+    },
+    duration: 8,
+    repeat: -1,
+    repeatDelay: 1,
+    ease: "none",
+  });
+
+  tween.progress(progress);
+};
+
+document.addEventListener("DOMContentLoaded", initMotionPath);
+window.addEventListener("resize", initMotionPath);
