d14411e5dd69c0412cf6a372a5140eecce22c677
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2023-01-10 14:23:19 -0800

    done?

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,31 @@
 import { gsap } from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
-gsap.registerPlugin(MotionPathPlugin);
-
-let tween;
-
 const initMotionPath = () => {
-  let progress = tween ? tween.progress() : 0;
-  tween && tween.progress(0).kill();
+  gsap.registerPlugin(MotionPathPlugin);
+  let tween;
 
-  tween = gsap.to("#dot", {
-    motionPath: {
-      path: "#path",
-      align: "#path",
-      alignOrigin: [0.5, 0.5],
-      autoRotate: true,
-    },
-    duration: 8,
-    repeat: -1,
-    repeatDelay: 1,
-    ease: "none",
-  });
+  const createTween = () => {
+    let progress = tween ? tween.progress() : 0;
+    tween && tween.progress(0).kill();
 
-  tween.progress(progress);
+    tween = gsap.to("#dot", {
+      motionPath: {
+        path: "#path",
+        align: "#path",
+        alignOrigin: [0.5, 0.5],
+        autoRotate: true,
+      },
+      duration: 8,
+      repeat: -1,
+      repeatDelay: 1,
+      ease: "none",
+    });
+
+    tween.progress(progress);
+  };
+  createTween();
+  window.addEventListener("resize", createTween);
 };
 
 document.addEventListener("DOMContentLoaded", initMotionPath);
-window.addEventListener("resize", initMotionPath);
